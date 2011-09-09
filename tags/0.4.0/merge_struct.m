98e6cf78fc3e7223988cf484903193033633c2fd
jirkacigler@gmail.com <jirkacigler@gmail.com@25aab069-d569-5166-cbf6-301d2de68a3e>
2011-09-09 07:19:37 +0000

    Version 0.4.0 created

---
+++ tags/0.4.0/merge_struct.m
@@ -0,0 +1,31 @@
+
+%--------------------------------------------------------------------------
+% Does merge of two structures. The result is structure which is union of
+% fields of p and s. If there are equal field names in p and s, fields in p
+% are overwriten with their peers from s.
+%
+function result = merge_struct(p, s, donotmerge)
+    if ~( isstruct(p) && isstruct(s) )
+        error('Only structures can be merged.');
+    end;
+    if ~exist('donotmerge','var')
+        donotmerge = {};
+    end
+    result = p;
+    for i = fields(s)'
+        fld = char(i);
+        if any(cellfun(@(x)isequal(x, fld), donotmerge))
+            continue;
+        end;
+ %       if isfield(result, fld)
+ %           % Just give the user a hint that there may be some information
+ %           % lost.
+ %           fprintf(['Overwriting field ',fld,'\n']);
+ %       end;
+        %disp('Assigning:')
+        %disp(['fieldname: ',fld]);
+        %disp(s.(fld));
+        %disp('----------');
+        result.(fld) = s.(fld);
+    end;
+end
