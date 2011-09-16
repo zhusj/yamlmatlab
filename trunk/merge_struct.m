0b3a9eaa134680e6628aed1336ed5a8b5d3da3cc
ptomask@gmail.com <ptomask@gmail.com@25aab069-d569-5166-cbf6-301d2de68a3e>
2011-09-16 15:33:56 +0000

    Fixed merging bug - Imports are now merged deeply.

--- trunk/merge_struct.m
+++ trunk/merge_struct.m
@@ -4,13 +4,19 @@
 % fields of p and s. If there are equal field names in p and s, fields in p
 % are overwriten with their peers from s.
 %
-function result = merge_struct(p, s, donotmerge)
+function result = merge_struct(p, s, donotmerge, deep)
     if ~( isstruct(p) && isstruct(s) )
         error('Only structures can be merged.');
     end;
     if ~exist('donotmerge','var')
         donotmerge = {};
     end
+    if ~exist('deep','var')
+        deep = 0;
+    elseif strcmp(deep, 'deep')
+        deep = 1;
+    end;
+    
     result = p;
     for i = fields(s)'
         fld = char(i);
@@ -26,6 +32,10 @@ function result = merge_struct(p, s, donotmerge)
         %disp(['fieldname: ',fld]);
         %disp(s.(fld));
         %disp('----------');
-        result.(fld) = s.(fld);
+        if deep == 1 && isfield(result, fld)
+            result.(fld) = merge_struct(result.(fld), s.(fld), donotmerge, deep);
+        else
+            result.(fld) = s.(fld);
+        end;
     end;
 end
