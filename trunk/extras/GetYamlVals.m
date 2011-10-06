1bdd499af22699ae5985074b4c2d820bed115203
jirkacigler@gmail.com <jirkacigler@gmail.com@25aab069-d569-5166-cbf6-301d2de68a3e>
2011-10-06 17:01:37 +0000

    fixed problem in getyamlvals

--- trunk/extras/GetYamlVals.m
+++ trunk/extras/GetYamlVals.m
@@ -5,14 +5,24 @@ function [vals, timeaxis] = GetYamlVals(yamldata)
 % obtain number of samples
 n = max(size(yamldata));
 
-% create output matrices
-timeaxis =  NaN*ones(n,1);
-if n % only if there are some elements of timeaxis
-    vals = NaN*ones(n,numel(yamldata{1})-1);
-end
-for i=1:n
-    timeaxis(i) = double(yamldata{i}{1});
-    vals(i,:)   = cell2mat(yamldata{i}(2:end));
-end
+if n
+    if not(iscell(yamldata{1}))
+        timeaxis = double(yamldata{1});
+        vals   = cell2mat(yamldata(2:end));
+    else
+
+        % create output matrices
+        timeaxis =  NaN*ones(n,1);
+        if n % only if there are some elements of timeaxis
+            vals = NaN*ones(n,numel(yamldata{1})-1);
+        end
+        for i=1:n
+            timeaxis(i) = double(yamldata{i}{1});
+            vals(i,:)   = cell2mat(yamldata{i}(2:end));
+        end
+    end
 
 end
+
+end % end of function
+
